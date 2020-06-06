2fd136ac61e66bd1249899a54b4417140f904880
YT <tagawa6520@gmail.com>
2020-06-06 03:58:24 +0000

    ブランチにプッシュ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html:"これはgit用のテストです"
+    render html:"これはgit用のテストですaaa"
   end
 end
